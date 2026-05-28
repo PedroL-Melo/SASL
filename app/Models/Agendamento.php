e22997ca2d6f8cc60288f896b5a8bcb72f9ec851
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-28 16:30:11 -0500

    Configurando model agendamentos

--- app/Models/Agendamento.php
+++ app/Models/Agendamento.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agendamento extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'sala_id',
+        'laboratorio_id',
+        'data_hora_inicio',
+        'data_hora_fim',
+        'status_agendamento',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sala()
+    {
+        return $this->belongsTo(Sala::class);
+    }
+
+    public function laboratorio()
+    {
+        return $this->belongsTo(Laboratorio::class);
+    }
 }
