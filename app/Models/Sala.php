40eff5ab0bfef059461d6a5a2aec37d086c800ad
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-05-28 16:34:41 -0500

    Configurando model salas

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -5,12 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Sala extends Model
-{
+{   
+    protected $table = 'salas';
     protected $fillable = [
         'nome',
         'capacidade',
         'bloco',
         'piso',
         'status_sala',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
     ];
 }
