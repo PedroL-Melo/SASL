20a291e9fdaad3a54cdbd3fbed561636a6aeb8d8
Pedro Melo <pedrolucasmelo5@gmail.com>
2026-06-15 15:33:46 -0500

    cadastro feito com sucesso

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\AgendamentoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
